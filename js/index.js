f7519b80c1ca2f284117d24407b4d1e227bf71a9
Mikhailo.Yu <miha.jewish@gmail.com>
2022-12-29 19:26:25 +0200

    init

---
+++ js/index.js
@@ -0,0 +1,5 @@
+let amountHours = prompt('Enter your number of hours to convert to seconds');
+
+let amountSeconds = amountHours * 3600;
+
+alert('Your result:' + '\n' + Number(amountSeconds) + 's');
